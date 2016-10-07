1bfc99a3266a41842d76f9e894ffa1e5b73b6f28
Tharidu Fernando <tharidufernando@gmail.com>
2016-10-07 12:09:16 +0200

    Filled the missing parts

--- lab3/spark/kafkaspark.scala
+++ lab3/spark/kafkaspark.scala
@@ -12,29 +12,33 @@ object KafkaWordCount {
   def main(args: Array[String]) {
 
     val kafkaConf = Map(
-	"metadata.broker.list" -> "localhost:9092",
-	"zookeeper.connect" -> "localhost:2181",
-	"group.id" -> "kafka-spark-streaming",
-	"zookeeper.connection.timeout.ms" -> "1000")
+    	"metadata.broker.list" -> "localhost:9092",
+    	"zookeeper.connect" -> "localhost:2181",
+    	"group.id" -> "kafka-spark-streaming",
+    	"zookeeper.connection.timeout.ms" -> "1000")
 
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster(<FILL IN>)
-    val ssc = new StreamingContext(<FILL IN>)
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
     // if you want to try the receiver-less approach, comment the below line and uncomment the next one
-    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](<FILL IN>)
-    //val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](<FILL IN>)
+    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](ssc, kafkaConf, Map("avg" -> 1), StorageLevel.MEMORY_ONLY)
+    // val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, Set("avg"))
 
-    val values = messages.map(<FILL IN>)
-    val pairs = values.<FILL IN>
+    val values = messages.map(pair => (pair._2.split(",")(0), (1, pair._2.split(",")(1).toInt)))
+    val pairs = values.reduceByKey((p1, p2) => (p1._1 + p2._1, p1._2 + p2._2))
 
-
-    def mappingFunc(key: String, value: Option[Double], state: State[Double]): Option[(String, Double)] = {
-	<FILL IN>
+    // State is the sum and the count of elements
+    def mappingFunc(key: String, value: Option[(Int, Int)], state: State[(Int, Int)]): Option[(String, Double)] = {
+      val current = state.getOption.getOrElse(0, 0)
+      val updatedValue = value.getOrElse(0, 0)
+    	val updated = (current._1 + updatedValue._1, current._2 + updatedValue._2) // Update the sum and no of elements
+      val output = (key, updated._2.toDouble / updated._1.toDouble) // Calculate the average
+      state.update(updated)
+      Some(output)
     }
 
-
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
 
     stateDstream.print()
     ssc.start()
